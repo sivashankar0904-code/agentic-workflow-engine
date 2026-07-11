c8cf4183826cbb451df2bb2884279a651e6494f4
= <=>
2026-07-12 00:08:27 +0530

    Remove React application files and assets

--- mock_services/mock_service_2/app/api/v1/messages.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from app.core.consumer import received_messages
-
-router = APIRouter(prefix="/messages", tags=["messages"])
-
-
-@router.get("/")
-def get_messages():
-    return {"service": "mock_service_2", "messages": received_messages}
